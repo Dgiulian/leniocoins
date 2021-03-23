b58244257e4891cb42b844f59cdf8e2b25352b2a
Diego Giuliani <giuliani.diego@gmail.com>
2021-03-22 21:44:03 -0300

    feat: Add coin logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
 import { Web3Context } from '../context/Web3Context'
+import CoinLogo from '../shared/shiny-coin.svg';
 
 function Navbar() {
     const { account } = useContext(Web3Context);
     return (
         <div className="bg-gray-300 ">
             <header className="container py-4 mx-auto flex justify-between align-middle">
-                <span >Lenio Coins</span>
+                <span className="text-xl" >
+                    <img src={CoinLogo} alt="Lenio Logo" className="w-6 mr-2" />  Lenio Coins</span>
                 <span className="text-gray-600">{account}</span>
             </header>
         </div>
